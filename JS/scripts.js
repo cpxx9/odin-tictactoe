b80421f590febdabb4c017f4c4748b4183f7ddf0
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-08 14:16:45 -0500

    added psuedocode to file to start laying out real code

---
+++ JS/scripts.js
@@ -0,0 +1,62 @@
+// module for game board
+function gameBoardModule() {
+  // store the board as an array
+  //  >initialize the rows and columns variables for the board
+  //  >initialize board array as empty
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  //  >push Cell object to array making 2d array
+  for (let i = 0; i < rows; i++) {
+
+  }
+  // create method to get the array
+  // create method to add user move to gameboard (x or o)
+  //  >checks to make sure move is valid
+  // create method to print gameboard to console so we can see what is happening
+  //  >will be removed later
+  
+  // return the 3 methods
+  
+}
+
+// factory for Cell update methods
+// represents one cell on the game board
+//  can have one of three:
+//   > 0: no x or o
+//   > 1: x
+//   > 2: o
+// initialize variable for value
+// create method to make the move
+// create method to get the move
+
+// return the 2 methods
+
+// module for game controller
+// Takes two players as parameters
+//  >controls flow and state of game turns
+// initialize board to gameboard module
+// create array of player objects
+//  >each player has a value for their marker (x or o)
+// set the active player to random player in the array
+
+// create method to switch the active player
+
+// create method to get the active player
+
+// create method to print the new round text
+//  >will show whose turn it is
+
+// create method to play the round
+//  >print what player made what move
+//  >store the move in the board with method on the board object
+//  >switch whose turn it is with the switch player method
+//  >print whose turn it is with the print round method
+
+//start the game
+// >call the print round method
+
+//return the method to get the active player and the play round method
+
+//store the controller object in a variable
