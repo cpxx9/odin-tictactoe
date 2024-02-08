6cfa3a7b63236fd7bfcd6f944e0a19fcace0917f
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-08 17:45:03 -0500

    added underscores to private variables

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,14 +3,14 @@ function gameBoardModule() {
   // store the board as an array
   //  initialize the rows and columns variables for the board
   //  initialize board array as empty
-  const rows = 3;
-  const columns = 3;
+  const _rows = 3;
+  const _columns = 3;
   const board = [];
 
   //  >push Cell object to array making 2d array
-  for (let i = 0; i < rows; i++) {
+  for (let i = 0; i < _rows; i++) {
     board[i] = [];
-    for (let n = 0; n < columns; n++) {
+    for (let n = 0; n < _columns; n++) {
       board[i].push(Cell());
     }
   }
@@ -49,11 +49,11 @@ function gameBoardModule() {
 //    2: o player
 function Cell() { 
   // initialize variable for marker value
-  let value = 0;
+  let _value = 0;
   // create method to make the move based on the player calling it
-  const addMarker = (player) => value = player;
+  const addMarker = (player) => _value = player;
   // create method to get the move
-  const getValue = () => value;
+  const getValue = () => _value;
   // return the 2 methods
   return {
     addMarker,
@@ -63,31 +63,30 @@ function Cell() {
 
 // module for game controller
 // Takes two players as parameters
-function GameControllerModule(playerOneName = "Player One", playerTwoName = "Player Two") {
-  
-}
 //  controls flow and state of game turns
-// initialize board to gameboard module
-// create array of player objects
-//  each player has a value for their marker (x or o)
-// set the active player to random player in the array
+function GameControllerModule(playerOneName = "Player One", playerTwoName = "Player Two") {
+  // initialize board to gameboard module
+  // create array of player objects
+  //  each player has a value for their marker (x or o)
+  // set the active player to random player in the array
 
-// create method to switch the active player
+  // create method to switch the active player
 
-// create method to get the active player
+  // create method to get the active player
 
-// create method to print the new round text
-//  will show whose turn it is
+  // create method to print the new round text
+  //  will show whose turn it is
 
-// create method to play the round
-//  print what player made what move
-//  store the move in the board with method on the board object
-//  switch whose turn it is with the switch player method
-//  print whose turn it is with the print round method
+  // create method to play the round
+  //  print what player made what move
+  //  store the move in the board with method on the board object
+  //  switch whose turn it is with the switch player method
+  //  print whose turn it is with the print round method
 
-// start the game
-//  call the print round method
+  // start the game
+  //  call the print round method
 
-// return the method to get the active player and the play round method
+  // return the method to get the active player and the play round method
+}
 
 // store the controller object in a variable
