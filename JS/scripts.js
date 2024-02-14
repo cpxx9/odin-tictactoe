0bc57e7eb54da21d26f3baab2dadb93ee85b4aae
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-14 13:53:13 -0500

    used prettier formatting

--- JS/scripts.js
+++ JS/scripts.js
@@ -12,54 +12,59 @@ function gameBoardModule() {
   }
 
   const getBoard = () => _board;
-  
+
   const placeMarker = (playerRow, playerColumn, player) => {
     let isMoveValid = false;
-    _board[playerRow][playerColumn].getValue() === 0 ? isMoveValid = true : isMoveValid = false;
-    if(isMoveValid) {
+    _board[playerRow][playerColumn].getValue() === 0
+      ? (isMoveValid = true)
+      : (isMoveValid = false);
+    if (isMoveValid) {
       _board[playerRow][playerColumn].addMarker(player);
     } else {
       return;
     }
-  }
-  
+  };
+
   return {
     getBoard,
-    placeMarker
-  }
+    placeMarker,
+  };
 }
 
 //    0: no x or o
 //    1: x player
 //    2: o player
-function Cell() { 
+function Cell() {
   let _marker = 0;
-  
-  const addMarker = (player) => _marker = player;
-  
+
+  const addMarker = (player) => (_marker = player);
+
   const getValue = () => _marker;
-  
+
   return {
     addMarker,
-    getValue
+    getValue,
   };
 }
 
 // module for game controller
-function GameControllerModule(playerOneName = "Player One", playerTwoName = "Player Two") {
+function GameControllerModule(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
   const _board = gameBoardModule();
   const players = [
     {
       _name: playerOneName,
-      _marker: 1
+      _marker: 1,
     },
     {
       _name: playerTwoName,
-      _marker: 2
-    }
+      _marker: 2,
+    },
   ];
   let _activePlayer = players[Math.floor(Math.random() * 2)];
-  
+
   const switchPlayerTurn = () => {
     _activePlayer = _activePlayer === players[0] ? players[1] : players[0];
   };
@@ -72,23 +77,37 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
 
   const horizontalCheck = (currentBoard) => {
     for (let i = 0; i < currentBoard.length; i++) {
-      if (currentBoard[i][0] === currentBoard[i][1] && currentBoard[i][1] === currentBoard[i][2] && currentBoard[i][i] != 0) {
-        return true;        
+      if (
+        currentBoard[i][0] === currentBoard[i][1] &&
+        currentBoard[i][1] === currentBoard[i][2] &&
+        currentBoard[i][i] != 0
+      ) {
+        return true;
       }
     }
-  }; 
+  };
 
   const verticleCheck = (currentBoard) => {
     for (let i = 0; i < currentBoard.length; i++) {
-      if (currentBoard[0][i] === currentBoard[1][i] && currentBoard[1][i] === currentBoard[2][i] && currentBoard[i][i] != 0) {
+      if (
+        currentBoard[0][i] === currentBoard[1][i] &&
+        currentBoard[1][i] === currentBoard[2][i] &&
+        currentBoard[i][i] != 0
+      ) {
         return true;
       }
     }
   };
 
   const crossCheck = (currentBoard) => {
-    if (currentBoard[1][1] != 0 && ((currentBoard[0][0] === currentBoard[1][1] && currentBoard[1][1] === currentBoard[2][2]) || (currentBoard[0][2] === currentBoard[1][1] && currentBoard[1][1] === currentBoard[2][0]))) {
-        return true;
+    if (
+      currentBoard[1][1] != 0 &&
+      ((currentBoard[0][0] === currentBoard[1][1] &&
+        currentBoard[1][1] === currentBoard[2][2]) ||
+        (currentBoard[0][2] === currentBoard[1][1] &&
+          currentBoard[1][1] === currentBoard[2][0]))
+    ) {
+      return true;
     }
   };
 
@@ -96,7 +115,7 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
     let emptyCells = 9;
     for (let i = 0; i < currentBoard.length; i++) {
       for (let n = 0; n < currentBoard[i].length; n++) {
-        currentBoard[i][n] != 0 ? emptyCells-- : emptyCells = emptyCells;
+        currentBoard[i][n] != 0 ? emptyCells-- : (emptyCells = emptyCells);
       }
     }
     if (emptyCells === 0) {
@@ -105,48 +124,54 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
   };
 
   const checkWinner = () => {
-    const _currentBoardValues = _board.getBoard().map((row) => row.map((cell) => cell.getValue()));
-    
-    if (horizontalCheck(_currentBoardValues) || verticleCheck(_currentBoardValues) || crossCheck(_currentBoardValues)){
-      return 'win';
+    const _currentBoardValues = _board
+      .getBoard()
+      .map((row) => row.map((cell) => cell.getValue()));
+
+    if (
+      horizontalCheck(_currentBoardValues) ||
+      verticleCheck(_currentBoardValues) ||
+      crossCheck(_currentBoardValues)
+    ) {
+      return "win";
     } else {
-      if(tieCheck(_currentBoardValues)) {
-        return 'tie';
+      if (tieCheck(_currentBoardValues)) {
+        return "tie";
       }
     }
   };
 
   const playRound = (row, column) => {
     _board.placeMarker(row, column, getActivePlayer()._marker);
-    
-    if(checkWinner() === 'win') {
-      return 'win';
-    } else if (checkWinner() === 'tie') {
-      return 'tie'
+
+    if (checkWinner() === "win") {
+      return "win";
+    } else if (checkWinner() === "tie") {
+      return "tie";
     } else {
       switchPlayerTurn();
-    }    
-  }
+    }
+  };
 
   return {
     getActivePlayer,
     changePlayerName,
     playRound,
-    getBoard: _board.getBoard
-  }
+    getBoard: _board.getBoard,
+  };
 }
 
 //module for DOM integration
 (function screenController() {
   let game = GameControllerModule();
   let players = [];
-  const playerCreationDOM = document.querySelector('.player-creation');
-  const boardDOM = document.querySelector('.board');
-  const turnDOM = document.querySelector('.turn');
-  const resetDOM = document.querySelector('.reset');
+  const playerCreationDOM = document.querySelector(".player-creation");
+  const boardDOM = document.querySelector(".board");
+  const turnDOM = document.querySelector(".turn");
+  const resetDOM = document.querySelector(".reset");
 
   const updateScreen = () => {
-    boardDOM.textContent = '';
+    boardDOM.textContent = "";
 
     const board = game.getBoard();
     const currentPlayer = game.getActivePlayer();
@@ -155,108 +180,106 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
 
     board.forEach((row, rowIndex) => {
       row.forEach((cell, columnIndex) => {
-        const gameButton = document.createElement('button');
-        gameButton.classList.add('cell');
+        const gameButton = document.createElement("button");
+        gameButton.classList.add("cell");
         gameButton.dataset.row = rowIndex;
         gameButton.dataset.column = columnIndex;
 
         const cellMarker = cell.getValue();
         switch (cellMarker) {
           case 1:
-            gameButton.textContent = 'X';
+            gameButton.textContent = "X";
             break;
           case 2:
-            gameButton.textContent = 'O';
+            gameButton.textContent = "O";
             break;
-          defualt:
-            gameButton.textContent = '';
+            defualt: gameButton.textContent = "";
         }
 
         boardDOM.appendChild(gameButton);
-      })
+      });
     });
   };
 
-  const restartButton = document.createElement('button');
+  const restartButton = document.createElement("button");
   const addResetButton = () => {
-    
-    restartButton.classList.add('restart-btn');
-    restartButton.classList.add('reset-btns');
+    restartButton.classList.add("restart-btn");
+    restartButton.classList.add("reset-btns");
     restartButton.textContent = "New players?";
-    restartButton.addEventListener('click', restartClickHandler);    
+    restartButton.addEventListener("click", restartClickHandler);
     resetDOM.appendChild(restartButton);
   };
 
-  const rematchButton = document.createElement('button');
+  const rematchButton = document.createElement("button");
   const endGame = (outcome) => {
-    if (outcome === 'tie' || outcome === 'win'){
-      let buttons = boardDOM.querySelectorAll('.cell');
+    if (outcome === "tie" || outcome === "win") {
+      let buttons = boardDOM.querySelectorAll(".cell");
       buttons.forEach((button) => {
         button.disabled = true;
       });
 
-      if (outcome === 'tie') {
+      if (outcome === "tie") {
         turnDOM.textContent = "It's a tie!";
-      } else if (outcome === 'win') {
+      } else if (outcome === "win") {
         const winner = game.getActivePlayer();
         turnDOM.textContent = `${winner._name} wins!`;
       }
 
-      rematchButton.classList.add('rematch-btn');
-      rematchButton.classList.add('reset-btns');
+      rematchButton.classList.add("rematch-btn");
+      rematchButton.classList.add("reset-btns");
       rematchButton.textContent = "Rematch?";
-      rematchButton.addEventListener('click', rematchClickHandler);
+      rematchButton.addEventListener("click", rematchClickHandler);
       resetDOM.appendChild(rematchButton);
     }
-  };  
+  };
 
   const getPlayers = (playerAmount) => {
-    let buttons = document.querySelectorAll('.cell');
-      buttons.forEach((button) => {
-        button.disabled = true;
-      });
-    
+    let buttons = document.querySelectorAll(".cell");
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+
     let playerCounter = 1;
-    const playerCreateLabel = document.createElement('label');
-    const playerCreateInput = document.createElement('input');
-    const playerCreateButton = document.createElement('button');
-    const cancelButton = document.createElement('button');
+    const playerCreateLabel = document.createElement("label");
+    const playerCreateInput = document.createElement("input");
+    const playerCreateButton = document.createElement("button");
+    const cancelButton = document.createElement("button");
     const updatePlayerInput = () => {
       playerCreateLabel.textContent = `Player ${playerCounter}'s name:`;
     };
 
-    playerCreateLabel.classList.add('creation-label');
-    playerCreateInput.classList.add('creation-input');    
-    playerCreateButton.classList.add('creation-button');    
+    playerCreateLabel.classList.add("creation-label");
+    playerCreateInput.classList.add("creation-input");
+    playerCreateButton.classList.add("creation-button");
 
-    updatePlayerInput();    
+    updatePlayerInput();
     playerCreateButton.textContent = "Submit";
     cancelButton.textContent = "Leave default";
 
     playerCreationDOM.appendChild(playerCreateLabel);
     playerCreationDOM.appendChild(playerCreateInput);
-    playerCreationDOM.appendChild(playerCreateButton);    
-    playerCreationDOM.appendChild(cancelButton);    
+    playerCreationDOM.appendChild(playerCreateButton);
+    playerCreationDOM.appendChild(cancelButton);
 
-    cancelButton.onclick = function() {
+    cancelButton.onclick = function () {
       playerCounter++;
       updatePlayerInput();
-      playerCreateInput.value = '';
+      playerCreateInput.value = "";
       if (playerCounter === playerAmount + 1) {
-        playerCreationDOM.innerHTML = '';
+        playerCreationDOM.innerHTML = "";
         restartButton.disabled = false;
         updateScreen();
       }
     };
 
-    playerCreateButton.onclick = function(){
-      game.changePlayerName((playerCounter - 1), playerCreateInput.value);
+    playerCreateButton.onclick = function () {
+      game.changePlayerName(playerCounter - 1, playerCreateInput.value);
       players.push(playerCreateInput.value);
       playerCounter++;
       updatePlayerInput();
-      playerCreateInput.value = '';
+      playerCreateInput.value = "";
       if (playerCounter === playerAmount + 1) {
-        playerCreationDOM.innerHTML = '';
+        playerCreationDOM.innerHTML = "";
         restartButton.disabled = false;
         updateScreen();
       }
@@ -266,8 +289,8 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
   function buttonClickHandler(e) {
     const selectedRow = e.target.dataset.row;
     const selectedColumn = e.target.dataset.column;
-    if(!selectedColumn || !selectedRow) return;
-    
+    if (!selectedColumn || !selectedRow) return;
+
     const round = game.playRound(selectedRow, selectedColumn);
 
     updateScreen();
@@ -275,12 +298,12 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
     endGame(round);
   }
 
-  boardDOM.addEventListener('click', buttonClickHandler);  
+  boardDOM.addEventListener("click", buttonClickHandler);
 
   function rematchClickHandler() {
-    game = GameControllerModule(players[0], players[1])
+    game = GameControllerModule(players[0], players[1]);
     updateScreen();
-    resetDOM.innerHTML = '';
+    resetDOM.innerHTML = "";
     addResetButton();
   }
 
@@ -290,7 +313,7 @@ function GameControllerModule(playerOneName = "Player One", playerTwoName = "Pla
     updateScreen();
     restartButton.disabled = true;
     getPlayers(2);
-    resetDOM.innerHTML = '';
+    resetDOM.innerHTML = "";
     addResetButton();
   }
 
