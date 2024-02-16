f1b0ee84f844ea91c279614cd40a1fd30c051372
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-15 21:38:03 -0500

    Fixed class to cancel button

--- JS/scripts.js
+++ JS/scripts.js
@@ -250,7 +250,7 @@ function GameControllerModule(
 
     playerCreateLabel.classList.add("creation-label");
     playerCreateInput.classList.add("creation-input");
-    cancelButton.classList.add("creation-input");
+    cancelButton.classList.add("creation-button");
     playerCreateButton.classList.add("creation-button");
 
     updatePlayerInput();
